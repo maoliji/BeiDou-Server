08054c6e3f4d0b70e094ebd52e488c6cc99274b5
maoliji <maopear@163.com>
2026-06-04 15:49:10 +0800

    将战神combo计时时间延长到7秒，降低战神续combo难度。

--- gms-server/src/main/java/org/gms/net/server/channel/handlers/AranComboHandler.java
+++ gms-server/src/main/java/org/gms/net/server/channel/handlers/AranComboHandler.java
@@ -38,7 +38,7 @@ public class AranComboHandler extends AbstractPacketHandler {
         if (GameConstants.isAran(player.getJob().getId()) && (skillLevel > 0 || player.getJob().getId() == 2000)) {
             final long currentTime = currentServerTime();
             short combo = player.getCombo();
-            if ((currentTime - player.getLastCombo()) > 3000 && combo > 0) {
+            if ((currentTime - player.getLastCombo()) > 7000 && combo > 0) {
                 combo = 0;
             }
             combo++;
